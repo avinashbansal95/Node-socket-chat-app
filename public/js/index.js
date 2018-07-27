7a791ac10944a09b126b5539c789c615dcc1f346
avinashbansal95 <avinashbansal95@gmail.com>
2018-07-27 15:46:27 +0530

    Add form and show msgs to browser

--- public/js/index.js
+++ public/js/index.js
@@ -12,12 +12,28 @@ console.log("Disconnected from the server")
 socket.on('newMessage', function (message) 
 {
 console.log('new Message',message);
+var li = jQuery('<li></li>');
+li.text(`${message.from} : ${message.text}`);
+jQuery('#messages').append(li);
 })
 
-socket.emit('createMessage',{
-    from:'avinashbansal',
-    text:'What is going on'
-},function(data)
+// socket.emit('createMessage',{
+//     from:'avinashbansal',
+//     text:'What is going on'
+// },function(data)
+// {
+//     console.log(data,':Got it')
+// });
+
+jQuery('#message-form').on('submit',function(e)
 {
-    console.log(data,':Got it')
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from:'avinashbansal',
+    text:jQuery('[name=message]').val()
+},function()
+{
+
+});
+
 });
